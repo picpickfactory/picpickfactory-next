442255ffe8a0aa7fb3840d9c2232443e6a077cf6
nabhansuwanachote <nabhan.au@gmail.com>
2024-07-05 23:04:15 +0700

    update slideshow

--- src/components/ImageGallery/index.tsx
+++ src/components/ImageGallery/index.tsx
@@ -1,31 +1,61 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { cn } from '@/utilities/tailwindUtils';
 export interface IimageGallery {
   imgUrl: string[];
   duration: number;
-  fadeInDuration?: number;
-  fadeOutDuration?: number;
-  isSlideShowActive: boolean;
+  autoSlideShow: boolean;
+  fadeInDuration?: string;
+  fadeOutDuration?: string;
   styling?: string[];
 }
 
-const ImageGallery: React.FC<IimageGallery> = ({ imgUrl, duration, fadeInDuration, fadeOutDuration, isSlideShowActive, styling }) => {
+const ImageGallery: React.FC<IimageGallery> = ({ imgUrl, duration, autoSlideShow, fadeInDuration, fadeOutDuration, styling }) => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const imageFadeInStyle = cn("opacity-100", fadeInDuration ? `duration-[${fadeInDuration}s]` : "duration-[1s]");
-  const imageFadeOutStyle = cn("opacity-0", fadeOutDuration ? `duration-[${fadeOutDuration}s]` : "duration-[1s]");
+  const [isSlideShowActive, setIsSlideShowActive] = useState<boolean>(autoSlideShow);
+  const timerRef = useRef<any>(null);
+  const imageFadeInStyle = cn("opacity-100", fadeInDuration ?? "duration-[3s]");
+  const imageFadeOutStyle = cn("opacity-0", fadeOutDuration ?? "duration-[5s]");
 
   useEffect(() => {
     if (isSlideShowActive) {
-      setTimeout(()=> {
-        if (currentIndex >= imgUrl.length - 1) {
-          setCurrentIndex(0)
-        } else {
-          setCurrentIndex(currentIndex + 1)
-        }
-        
-      }, duration * 1000)
+      timerRef.current = setTimeout(() => {
+        setCurrentIndex((prevIndex) => (prevIndex >= imgUrl.length - 1 ? 0 : prevIndex + 1));
+      }, duration * 1000);
     }
-  })
+
+    return () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+      }
+    };
+  }, [isSlideShowActive, currentIndex, duration, imgUrl.length]);
+
+  const startSlideShow = () => {
+    setIsSlideShowActive(true);
+  };
+
+  const pauseSlideShow = () => {
+    setIsSlideShowActive(false);
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+  };
+
+  const handleSwipeLeft = () => {
+    if (currentIndex >= imgUrl.length - 1) {
+      setCurrentIndex(0)
+    } else {
+      setCurrentIndex(currentIndex + 1)
+    }
+  }
+
+  const handleSwipeRight = () => {
+    if (currentIndex <= 0) {
+      setCurrentIndex(imgUrl.length - 1)
+    } else {
+      setCurrentIndex(currentIndex - 1)
+    }
+  }
 
   return (
     <div className={cn("relative z-0 flex", styling)}>
