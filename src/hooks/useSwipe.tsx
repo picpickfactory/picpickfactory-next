31a9b609a26042c523acc9c08a6a0fc43aee5f5e
nabhansuwanachote <nabhan.au@gmail.com>
2024-07-05 20:21:59 +0700

    add swipe hook

---
+++ src/hooks/useSwipe.tsx
@@ -0,0 +1,38 @@
+import { on } from "events"
+import { useState } from "react"
+
+export default function useSwipe(minDistance: number = 50, onSwipeLeft: () => void, onSwipeRight: () => void){
+  const [touchStart, setTouchStart] = useState(NaN)
+  const [touchEnd, setTouchEnd] = useState(NaN)
+  const [moveDistance, setMoveDistance] = useState(0)
+
+  const handleTouchStart = (e: any) => {
+    setTouchEnd(NaN)
+    setTouchStart(e.targetTouches[0].clientX)
+  }
+
+  const handleTouchMove = (e: any) => {
+    if (!Number.isNaN(touchStart)) {
+      setMoveDistance(e.targetTouches[0].clientX - touchStart)
+    }
+    setTouchEnd(e.targetTouches[0].clientX)
+  }
+
+  const handleTouchEnd = () => {
+    if (Number.isNaN(touchStart) || Number.isNaN(touchEnd)) return
+    if (Math.abs(moveDistance) > minDistance) {
+      if (touchEnd > touchStart) {
+        onSwipeRight()
+      } else {
+        onSwipeLeft()
+      }
+    }
+  }
+
+  return {
+    handleTouchStart,
+    handleTouchMove,
+    handleTouchEnd,
+    moveDistance
+  }
+}
