6c58ff5f5b9be4a4b00ee5d81b87160e9738f570
leenawatH <leenawat.hon@student.mahidol.ac.th>
2024-07-12 04:18:03 +0700

    add data v1

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -48,44 +48,111 @@ const bbk = [
 
 const personal = [
     {
-      title: "Red",
-      path: "/red",
-      description: "",
-      url: [
-        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/Red-004.jpg?alt=media&token=f3640af6-43d6-4398-ac4c-dd25df69aa52",
-        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/Red-019.jpg?alt=media&token=d88fddc1-c735-4eeb-b6d0-4c5cd2a5474d",
-        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/Red-028.jpg?alt=media&token=d9d6304e-c1ad-486d-8dcb-d6957a715082",
-        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/Red-030.jpg?alt=media&token=3e0f9f61-30ad-4427-8727-28298facb911",
-      ],
+      "title": "BANGKOK CIRCUS 1",
+      "path": "/bangkok-circus-1",
+      "description": "The concept of merging the circus with the essence of Bangkok and extending it into a fashion photoshoot series called \"Bangkok Circus.\" This presentation is in a fun format and compares it to being a city, the city of Bangkok, from my perspective of being bored, crowded, and not very pleasant, but with the addition of lively characters it create a contrast.",
+      "url": [
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FBANGKOK%20CIRCUS%201%2FNone-052.jpg?alt=media&token=7043cb59-6d06-48c9-bdfc-0028ccafca06",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FBANGKOK%20CIRCUS%201%2FNone-006.jpg?alt=media&token=61759577-3154-49b7-aef3-11a0cacb48c4",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FBANGKOK%20CIRCUS%201%2FNone-009.jpg?alt=media&token=68f4371b-2bdf-49b9-bba5-c065ba2941cd",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FBANGKOK%20CIRCUS%201%2FNone-051.jpg?alt=media&token=1c4ccad2-a132-4d4e-afd0-42aa85d3f728",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FBANGKOK%20CIRCUS%201%2FNone-053.jpg?alt=media&token=0b336e98-c3f3-401c-ad60-942d913d6554",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FBANGKOK%20CIRCUS%201%2FNone-054.jpg?alt=media&token=9b678f20-2976-493b-85c0-27deecefef96",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FBANGKOK%20CIRCUS%201%2FNone-055.jpg?alt=media&token=e589cdd8-1faa-41ff-8a1e-d5a803a998de",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FBANGKOK%20CIRCUS%201%2FNone-057-Recovered.jpg?alt=media&token=6af3c63a-692c-4a4b-a6bf-fbe5de12bfe6",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FBANGKOK%20CIRCUS%201%2FNone-058.jpg?alt=media&token=723218cf-a259-4178-8d26-ec5d038ca81e"
+      ]
     },
     {
-        title: "S1",
-        path: "/s1",
-        description: "",
-        url: [
-          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/S1-023.jpg?alt=media&token=a810daf3-50e3-42e6-930c-f6e8955fbf1f",
-        ],
-      },
+      "title": "BANGKOK CIRCUS 2",
+      "path": "/bangkok-circus-2",
+      "description": "The concept of merging the circus with the essence of Bangkok and extending it into a fashion photoshoot series called \"Bangkok Circus.\" This presentation is in a fun format and compares it to being a city, the city of Bangkok, from my perspective of being bored, crowded, and not very pleasant, but with the addition of lively characters it create a contrast.",
+      "url": [
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FBANGKOK%20CIRCUS%202%2FS1-115.jpg?alt=media&token=3dde4357-f871-498d-99b6-49ae11f6ebed",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FBANGKOK%20CIRCUS%202%2FS2-172.jpg?alt=media&token=8844dcfb-4c67-4278-aae2-e76c19254ce3",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FBANGKOK%20CIRCUS%202%2FS3-214.jpg?alt=media&token=1b2d3d2f-26bf-4639-b424-4b78eca98334",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FBANGKOK%20CIRCUS%202%2FS4-305.jpg?alt=media&token=5a581d24-6d70-4b02-a07b-ac030cc3e22b",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FBANGKOK%20CIRCUS%202%2FS5-421.jpg?alt=media&token=b6da6d32-64ce-4d50-8957-0b13e25327bb",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FBANGKOK%20CIRCUS%202%2FS6-477.jpg?alt=media&token=04ad7c05-ff60-41b6-bae3-bdf3d7cc9c91",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FBANGKOK%20CIRCUS%202%2FS7-560.jpg?alt=media&token=c40fae92-7aa7-45a3-bdf5-d353e1f1180e",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FBANGKOK%20CIRCUS%202%2FS7-577.jpg?alt=media&token=e7cfae3a-fb85-4d76-b18b-c0288a46214e",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FBANGKOK%20CIRCUS%202%2FS8-697.jpg?alt=media&token=d57c071a-0067-47ef-a002-5fb7ba87abc9",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FBANGKOK%20CIRCUS%202%2FS9-897.jpg?alt=media&token=a977eff5-a90b-4cb2-9b01-e256f09adcf9"
+      ]
+    },
+    {
+      "title": "\"CLASSIC WOMEN 2023\"",
+      "path": "/classic-women-2023",
+      "description": "",
+      "url": [
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2F%22CLASSIC%20WOMEN%202023%22%2FRed-137.jpg?alt=media&token=cedc2b74-e200-47af-a815-3e5d0eedd8f2",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2F%22CLASSIC%20WOMEN%202023%22%2FRed-045.jpg?alt=media&token=e95c7e61-eb61-4263-9464-056982f1b152",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2F%22CLASSIC%20WOMEN%202023%22%2FRed-076.jpg?alt=media&token=083ade7c-014e-4273-9b32-7eaf5dbc5767",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2F%22CLASSIC%20WOMEN%202023%22%2FRed-144.jpg?alt=media&token=49c117a8-4fb2-4675-a061-75e2917ce29f",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2F%22CLASSIC%20WOMEN%202023%22%2FRed-148.jpg?alt=media&token=2d838c99-7ded-4b9a-8016-54982319a379"
+      ]
+    },
+    {
+      "title": "MASAN TAIPEI 2024",
+      "path": "/masan-taipei-2024",
+      "description": "",
+      "url": [
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FMASAN%20TAIPEI%202024%2FRed-017.jpg?alt=media&token=c5c072e7-740c-462d-9438-74ef208effd9",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FMASAN%20TAIPEI%202024%2FRed-011.jpg?alt=media&token=2ad6172d-89db-425c-8003-7357426d2614",
+        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/personal%2FMASAN%20TAIPEI%202024%2FRed-031.jpg?alt=media&token=f1f87de5-e413-454a-859b-9baf412cd393"
+      ]
+    },
 ];
 
 const commissioned = [
     {
-      title: "Bangkok Circus",
-      path: "/bangkok-circus",
-      description: "BKK Circus is a group of young artists who are passionate about circus arts. They have been performing in various events and festivals in Thailand and abroad. They are known for their unique style and creativity. Their performances are a mix of acrobatics, juggling, and other circus acts. They are always looking for new ways to entertain and inspire their audience.",
-      url: [
-        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/BKKCIRCUS01.jpg?alt=media&token=1bca2fbf-2902-4761-945f-167dc12200cd",
-        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/BKKCIRCUS02.jpg?alt=media&token=4a06006c-aee9-4218-99e4-0ce38a6e3a42",
-        "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/BKKCIRCUSLONGWAY.jpg?alt=media&token=d68d460f-9480-43cc-85c6-11c44afcb447",
-      ],
-    },
-    {
-        title: "London Brown",
+        title: "LONDONBROWN",
         path: "/london-brown",
-        description: "London Brown is a group of young artists who are passionate about circus arts. They have been performing in various events and festivals in Thailand and abroad. They are known for their unique style and creativity. Their performances are a mix of acrobatics, juggling, and other circus acts. They are always looking for new ways to entertain and inspire their audience.",
+        description: "",
         url: [
           "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/LB.jpg?alt=media&token=b41e836a-cdc4-4041-ad8e-091a0ee70d67",
         ],
+      },
+      {
+        title: "MERCHIS WITH TRUEBLUETATTOOO",
+        path: "/merchis-with-truebluetattooo",
+        description: "",
+        url: [
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FMERCHIS%20WITH%20TRUEBLUETATTOOO%2FIMG_7391.JPG?alt=media&token=f704b016-1466-436a-848d-2a8e72762bc6",
+        ],
+      },
+      {
+        "title": "MONROECLUBS",
+        "path": "/monroeclubs",
+        "description": "",
+        "url": [
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FMONROECLUBS%2FMONROECLUBS.jpg?alt=media&token=86cabbac-298e-4e4c-b165-abfc917abfd1",
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FMONROECLUBS%2FGreen-102.jpg?alt=media&token=bc44d217-6554-4d6d-9d89-1268b1ee0492",
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FMONROECLUBS%2FRed-035.jpg?alt=media&token=1560e15d-c532-4640-9d32-f1ccd93271ef",
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FMONROECLUBS%2FRed-047.jpg?alt=media&token=90df5f68-07b9-4922-98a1-e33c7179772b",
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FMONROECLUBS%2FRed-080.jpg?alt=media&token=41d50857-19fa-4ec5-9ae1-78387be1b6d4"
+        ]
+      },
+      {
+        "title": "ZAO_NUI",
+        "path": "/zao-nui",
+        "description": "",
+        "url": [
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FZAO_NUI%2FUntitled-2.jpg?alt=media&token=39567b36-be8f-4224-8c1c-7c5418ded98c",
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FZAO_NUI%2FRed-001.jpg?alt=media&token=3ec03e92-f426-4d5c-9662-ca2175ecf3fb",
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FZAO_NUI%2FRed-002.jpg?alt=media&token=6924184f-78bd-4bbb-b990-2aea8bd83b4a",
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FZAO_NUI%2FRed-003.jpg?alt=media&token=9790cd50-d16a-4127-b758-17169d672ff0",
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FZAO_NUI%2FRed-004.jpg?alt=media&token=97defa9f-f1cd-4dcd-9251-3133ccce8582",
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FZAO_NUI%2FRed-005.jpg?alt=media&token=a5c3ac3b-77d1-4682-a956-3dbef5a6c72e",
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FZAO_NUI%2FRed-006.jpg?alt=media&token=2be7396d-d13c-4155-bed0-33ba0fd5f515",
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FZAO_NUI%2FRed-007.jpg?alt=media&token=49b4e3ce-234b-4a28-8059-3ecf2479dc95",
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FZAO_NUI%2FRed-008.jpg?alt=media&token=7f599e18-b279-44e2-904e-34f6fc4a9ad5",
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FZAO_NUI%2FRed-009.jpg?alt=media&token=29f662d2-6f50-498f-b373-0fd049c513a1",
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FZAO_NUI%2FRed-010.jpg?alt=media&token=09629301-6ad1-46f3-bbe4-8ada46ea834a",
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FZAO_NUI%2FRed-011.jpg?alt=media&token=1911a36c-1e8d-473f-8ee0-ed7d019597cf",
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FZAO_NUI%2FRed-012.jpg?alt=media&token=eae7d65a-56c7-4952-a247-50dfef14187f",
+          "https://firebasestorage.googleapis.com/v0/b/pic-pick-factory.appspot.com/o/commissioned%2FZAO_NUI%2FRed-013.jpg?alt=media&token=13237fa3-e73a-4b01-9d31-7ef6de383899"
+        ]
       },
 ];
 
