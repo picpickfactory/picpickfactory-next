f4bd65e1eae95513f44fe9ce94b2e455d533691c
leenawatH <leenawat.hon@student.mahidol.ac.th>
2024-07-12 17:45:43 +0700

    add contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Box, Typography } from "@mui/material";
+
+export default function Page() {
+    return (
+        <Box className="mx-6 my-9">
+            <Typography>
+                Contact
+            </Typography>
+            <Typography className="mx-5 my-5 text-sm">
+                Contact <br /> 
+                Tel (+66)804415753<br />
+                picpickfactory@gmail.com<br /><br />
+                Instragram : Picpickfactory
+            </Typography>
+        </Box>
+    );
+}
