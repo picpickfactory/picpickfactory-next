a37e984869e999c3bf62d6fefafdea1b103abb27
leenawatH <leenawat.hon@student.mahidol.ac.th>
2024-07-12 15:04:28 +0700

    fix about us page

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -5,7 +5,7 @@ import { Box, Typography } from "@mui/material";
 export default function Page() {
     return (
         <Box className="mx-6 my-9">
-            <Typography className="">
+            <Typography>
                  About US
             </Typography>
             <Typography className="mx-5 my-5 text-sm">
